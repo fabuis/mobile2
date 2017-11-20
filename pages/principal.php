7bb0aedf2e5c0c05b8fccf8fc233e39b3d3433c5
Satel Importadores <satelimportadores@gmail.com>
2017-11-20 14:25:12 -0500

    Ortografia

--- pages/principal.php
+++ pages/principal.php
@@ -369,7 +369,7 @@ if($_SESSION["autentica"] != "OK")
                     <div class="col-lg-6">
                                   <div class="card mb-3">
                                 <div class="card-header">
-                                  <i class="fa fa-pie-chart"></i><h2>Celular mas vendido en Colombia TOP 5 </h2></div>
+                                  <i class="fa fa-pie-chart"></i><h2>Celular mas vendido en Colombia TOP 10 </h2></div>
                                 <div class="card-body">
                                   <canvas id="myPieChart" width="100%" height="100"></canvas>
                                 </div>
